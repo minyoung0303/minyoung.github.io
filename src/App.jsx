a83756ce8392d2569d6d2ba8b2ce07f80f40af73
chorongfire33 <choyeon8317@naver.com>
2025-12-16 23:33:42 +0900

    docs: React 기반 포트폴리오 환경설정

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="main">Main</section>
+      <section id="about">About</section>
+      <section id="skills">Skills</section>
+      <section id="projects">Projects</section>
+      <section id="contact">Contact</section>
+    </>
+  )
+}
+
+export default App
+
